a677d4ae8ac42a7a884c63f110d7d576adade715
Joqsa <114524842+Joqsa@users.noreply.github.com>
2022-10-23 15:31:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("PTH");
+
+function audioPlay() {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.2;
+    audio.play()
+}
+
+function videoPlay() {
+    var video = document.getElementById("video");
+    video.play()
+}
---
+++ typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed', {
+    strings: ['PTH', 'developer', 'scripter', '.gg/sulfate'],
+    typeSpeed: 40,
+    delaySpeed: 90,
+    showCursor: false,
+    loop: true
+});
